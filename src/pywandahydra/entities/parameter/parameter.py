ab6e34dbaead3e3bad94801dae3c1abace3606e0
samvanderzwan <sam.vanderzwan@deltares.nl>
2024-09-17 14:47:39 +0200

    Added first basic abstract classes

---
+++ src/pywandahydra/entities/parameter/parameter.py
@@ -0,0 +1,12 @@
+"""Module containing the parameter abstract class to be used to store in/output parameters."""
+
+from abc import ABC, abstractmethod
+
+
+class ParameterAbstract(ABC):
+    """Abstract class to be used for storing parameters."""
+
+    @abstractmethod
+    def __init__(self):
+        """Initializes the ParameterAbstract object."""
+        pass
