1188135ca12a75b11a738c72e6c82d5c2e75373c
LuisFelipe-PE <luis.penuelaescobar@deltares.nl>
2024-10-17 11:33:36 +0200

    Update pyproject toml and env
    Updated the pyproject toml to correctly install the required packages.

--- ci/win32/create_venv.cmd
+++ ci/win32/create_venv.cmd
@@ -1,9 +1,15 @@
 rem Short script to initialize virtual environment using venv and pip
 rem @echo off
-
+ 
 pushd .
 cd /D "%~dp0"
-py -3.11 -m venv ..\..\venv
+py -3.10 -m venv ..\..\venv
 call ..\..\venv\Scripts\activate.bat
 python -m pip install pip-tools
+call .\update_dependencies.cmd
+call .\install_dependencies.cmd
+rem Go to the package directory to call pip install -e .
+cd ..\..
+py -m pip install -e .
+call .\venv\Scripts\deactivate.bat
 popd
